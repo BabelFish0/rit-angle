14e1b9f856b2b05270294a06841dff7bc7b9c653
Jude Young <jude.d.l.young@icloud.com>
2022-12-06 16:26:37 +0000

    added code

--- final_prob/final_prob.py
+++ final_prob/final_prob.py
@@ -1,6 +1,7 @@
 import math
 
 primes = []
+a_primes = []
 
 def is_prime(num):
     for div in range(2, int(math.sqrt(num))+1):
@@ -19,10 +20,30 @@ while test_num <= 20000:
 def sum_digits(num):
     num = str(num)
     total = 0
-    for i in range(0, len(num)-1):
+    for i in range(0, len(num)):
         total += int(num[i])
     return total
 
-
+def prod_digits(num):
+    num = str(num)
+    total = 1
+    for i in range(0, len(num)):
+        total = total * int(num[i])
+    return total
 
 def is_am(num):
+    dig_sum = sum_digits(num)
+    dig_prod = prod_digits(num)
+    return num + dig_sum + dig_prod
+
+def is_a(num):
+    dig_sum = sum_digits(num)
+    return num + dig_sum
+
+for i in range(len(primes)-1):
+    if is_a(primes[i]) == primes[i+1]:
+        a_primes.append(primes[i])
+
+for i in range(len(primes)-1):
+    if is_am(primes[i]) == primes[i+1]:
+        print(primes[i])
