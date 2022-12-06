3f847c4e7b659360d8217074a0b02c96294d8197
Jude Young <jude.d.l.young@icloud.com>
2022-12-06 21:51:03 +0000

    first part final q

---
+++ final_q/finalq.py
@@ -0,0 +1,15 @@
+import math
+
+def test_integer(a, b, c, n):
+    return a**2 + b**2 + c**2 == n
+
+def find_abc(n):
+    out = []
+    for a in range(int(math.sqrt(n))+1):
+        for b in range(int(math.sqrt(n))+1):
+            for c in range(int(math.sqrt(n))+1):
+                if test_integer(a, b, c, n):
+                    out.append([a, b, c])
+    return out
+
+
